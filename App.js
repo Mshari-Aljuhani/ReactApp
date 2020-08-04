83d8787c16b49a7931dcb33c5aa5e64b6dc3f4a3
MshariAljuhani <Mshari.Aljuhani0@gmail.com>
2020-08-04 16:27:29 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  console.log("App executed")
+  
+  
+  return (
+    <View style={styles.container}>
+      <Text>  نقطة البداية</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
