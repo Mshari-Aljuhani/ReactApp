19a213f9f34332cd4b21d2f1d36bf7eec6b64586
MshariAljuhani <Mshari.Aljuhani0@gmail.com>
2020-08-05 02:43:29 +0300

    First Step

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
 
 export default function App() {
 
-  console.log("App executed")
+    return (
+      <View style={styles.container}>
+        <Text>hello world</Text>
+        <StatusBar style="auto" />
+        <Image source={{width:200, height:300, uri:"https://picsum.photos/200/300?grayscale"}} />
+      <Button title="اضغط هنا" onPress={() => 
+       Alert.alert("الزر انضغط","my massage", [
+          {text: "نعم", onPress:()=> console.log("yes")},
+          {text: "لا", onPress:() => console.log("No")},
+        ])}/>
+      
+      </View>
+    );
+  }
   
   
-  return (
-    <View style={styles.container}>
-      <Text>  نقطة البداية</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#B8C4C2',
+      alignItems: 'center',
+      justifyContent: 'center',
+      
+    },
+  });
